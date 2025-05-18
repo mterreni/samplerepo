30ab8fa663e5b37a9051e967037a7515c9242a27
mterreni <mterreni@users.noreply.github.com>
2025-05-18 14:10:27 +0300

    deposits
    deposits

---
+++ deposits/DDL_BASE/customers_ddl.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `me-sb-dgcp-dpoc-data-pr.CICD_Management.customers`
+(
+  customer_id INT64 NOT NULL,
+  first_name STRING,
+  last_name STRING,
+  email STRING,
+  phone_number STRING,
+  street STRING,
+  city STRING,
+  state STRING,
+  zip_code INT64,
+  registration_date TIMESTAMP
+);
